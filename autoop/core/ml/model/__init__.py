4863cbc61a711f2b0899fc96afb8e2adc5234c28
Matthias Pieper <m.pieper.4@student.rug.nl>
2024-11-10 16:58:36 +0100

    model's __init.py flake 8 check

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,17 +1,12 @@
-
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.classification.k_nearest_nerighbors import KNN
 
 
-REGRESSION_MODELS = [
-] # add your models as str here
+REGRESSION_MODELS = []
+# add your models as str here
 
-CLASSIFICATION_MODELS = [
-    "KNN",
-    "Logistic_Regression",
-    "GradientBoosting"
-    
-] # add your models as str here
+CLASSIFICATION_MODELS = []
+# add your models as str here
+
 
 def get_model(model_name: str) -> Model:
     """Factory function to get a model by name."""
